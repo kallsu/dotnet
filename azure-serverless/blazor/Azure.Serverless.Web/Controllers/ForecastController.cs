44b97a5b0aadac12a84d41a01e06ef611102831b
Giorgio Desideri <giorgio@7peakssoftware.com>
2020-03-09 19:56:10 +0700

    Serverless

---
+++ azure-serverless/blazor/Azure.Serverless.Web/Controllers/ForecastController.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Serverless.Web.Managers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Azure.Serverless.Web.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ForecastController : ControllerBase
+    {
+        private readonly WeatherForecastManager _manager;
+
+        public ForecastController() {
+            this._manager = new WeatherForecastManager();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherForecasts() 
+        {
+            var results = await _manager.GetForecasts();
+
+            return Ok(results);
+        }
+    }
+}
---
+++ azure-serverless/blazor/Azure.Serverless.Web/Managers/WeatherForecastManager.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Serverless.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Azure.Serverless.Web.Managers
+{
+    public class WeatherForecastManager
+    {
+        private static readonly Random GetRandom = new Random();
+
+        public WeatherForecastManager()
+        {
+        }
+
+        public async Task<IList<WeatherForecast>> GetForecasts()
+        {
+            var totalListSize = GetRandom.Next(10, 40);
+            var now = DateTime.UtcNow;
+
+            var forecasts = new List<WeatherForecast>(0);
+
+            for (var i = 0; i < totalListSize; i++)
+            {
+
+                var min = GetRandom.Next(-10, 18);
+                var max = GetRandom.Next(19, 40);
+
+                forecasts.Add(
+                new WeatherForecast()
+                {
+                    Date = now.AddHours(i),
+                    TemperatureC = GetRandom.Next(min, max),
+                    Summary = $"Weather between [{min}, {max}]"
+                });
+            }
+
+            return forecasts;
+        }
+    }
+}
