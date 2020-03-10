0b5746deffc3422df908493b21ae37d1443c2ac4
Giorgio Desideri <giorgio@7peakssoftware.com>
2020-03-10 18:22:26 +0700

    add lambdas

---
+++ aws-serverless/Aws.Serverless.Lambda/src/Dtac.Ev.Lambdas/ILambdaConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Dtac.Ev.Lambdas
+{
+    public interface ILambdaConfiguration
+    {
+        IConfigurationRoot Configuration { get; }
+    }
+}
